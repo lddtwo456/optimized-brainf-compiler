52beaa36b323fdfa1700a4a3e073e328a163f5dc
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2024-03-31 23:24:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import sys
 
+def getBin(num, length):
+  return format(num, '0{}b'.format(length))
+
+def getInt(bin):
+  return int(bin, 2)
+
 class bin:
-  def __init__(self, word, readable):
+  def __init__(self, words=None, readables=None):
     self.words = []
-    self.words.append([word, readable])
-  
+
+    if words != None:
+      for i in range(len(words)):
+        self.words.append([getInt(words[i]), readables[i], len(words[i])])
+
+  def __getitem__(self, i):
+    return self.words[i]
+    
   def printAsBin(self):
     for word in self.words:
-      sys.stdout.write(word[0]+" ")
+      sys.stdout.write(getBin(word[0], word[2])+" ")
     print("")
 
   def printAsReadable(self):
@@ -16,4 +28,29 @@ class bin:
     print("")
 
   def addWord(self, word, readable):
-    self.words.append([word, readable])
+    self.words.append([word, readable, len(word)])
+
+code = input("code: ")
+
+# get binary of code, like ingame
+bfCode = bin()
+for char in code:
+  if char == ".":
+    bfCode.addWord("001", "OUT")
+  elif char == ">":
+    bfCode.addWord("010", "RIGHT")
+  elif char == "<":
+    bfCode.addWord("011", "LEFT")
+  elif char == "+":
+    bfCode.addWord("100", "ADD")
+  elif char == "-":
+    bfCode.addWord("101", "SUB")
+  elif char == "[":
+    bfCode.addWord("110", "OPEN")
+  elif char == "]":
+    bfCode.addWord("111", "CLOSE")
+
+binary = bin(["000", "01011"], ["black", "yellow"])
+
+binary.printAsBin()
+binary.printAsReadable()
